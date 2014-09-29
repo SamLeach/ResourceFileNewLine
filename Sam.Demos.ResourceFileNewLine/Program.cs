e7d120eb5e0725e7dae35fbb69375e3d0fdd4b0d
Sam Leach <leachsam@gmail.com>
2014-09-29 10:32:39 +0200

    Demo the use of new lines in resources

---
+++ Sam.Demos.ResourceFileNewLine/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sam.Demos.ResourceFileNewLine
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Resources.OkMessage);
+            Console.WriteLine(Resources.OkMessageWithBrokenNewLine);
+            Console.WriteLine(Resources.OkMessageWithCorrectNewLine);
+
+            Console.ReadKey();
+        }
+    }
+}
